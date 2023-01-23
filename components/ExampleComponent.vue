a0a6a8b9698e80a300e4bf9b571f1fa4e3ddebd3
Tobias Lebeitsuk <tobias.lebeitsuk@gmail.com>
2023-01-23 13:25:29 +0200

    prisma

--- components/ExampleComponent.vue
+++ components/ExampleComponent.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 // ⚡️ Reactivity Transform: syntax magic to remove the need for .value everywhere
-const title = $ref('Pokemon')
+const title = $ref('Database')
 
 // ⚡️ Autocompleted API routes, data and types. Pick only the data you need.
-const { data } = await useFetch('/api/pokemon', { pick: ['name', 'sprites'] })
+const { data } = await useFetch('/api/db/1')
+// const { data } = await useFetch('/api/pokemon', { pick: ['name', 'sprites'] })
+
+onMounted(async () => {
+  await useFetch('/api/db/1', {
+    method: 'POST',
+    body: { description: 'Hello World', details: 'Test' },
+  })
+})
 </script>
 
 <template>
@@ -11,8 +19,7 @@ const { data } = await useFetch('/api/pokemon', { pick: ['name', 'sprites'] })
     <h3 class="text-lg">Component: {{ title }}</h3>
 
     <div class="flex flex-col justify-center items-center mt-8">
-      <div class="font-bold capitalize">{{ data?.name }}</div>
-      <img :src="data?.sprites.front_default" />
+      <div class="font-bold capitalize">{{ data }}</div>
     </div>
   </div>
 </template>
